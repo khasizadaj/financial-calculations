dc7c8326f9e04e5538b98441206d559b3153d7c3
Javid Khasizada <javid.khasizada@gmail.com>
2021-10-11 04:16:29 +0200

    moved file to correct folder; imports corrected

--- tests/test_interest_factors.py
+++ tests/test_interest_factors.py
@@ -5,25 +5,27 @@ TODO write unit tests for this module.
 """
 
 from decimal import Decimal
-import interest_factors
-import helper_funcs as hf
+from fin_funcs.interest_factors import (pvifa, pvifad)
+import fin_funcs.helper_funcs as hf
 
+# annuity factor
 return_rate = Decimal("0.02")
 number_of_years = 1
-pvifa = interest_factors.pvifa(return_rate, number_of_years)
+int_factor = pvifa(return_rate, number_of_years)
 assert hf.quantize(pvifa, 3) == Decimal("0.980")
 
 return_rate = Decimal("0.07")
 number_of_years = 7
-pvifa = interest_factors.pvifa(return_rate, number_of_years)
+int_factor = pvifa(return_rate, number_of_years)
 assert hf.quantize(pvifa, 3) == Decimal("5.389")
 
 return_rate = Decimal("0.14")
 number_of_years = 12
-pvifa = interest_factors.pvifa(return_rate, number_of_years)
+int_factor = pvifa(return_rate, number_of_years)
 assert hf.quantize(pvifa, 3) == Decimal("5.660")
 
+# annuity due factor
 return_rate = Decimal("0.10")
 number_of_years = 3
-pvifad = interest_factors.pvifad(return_rate, number_of_years)
+int_factor = pvifad(return_rate, number_of_years)
 assert hf.quantize(pvifad, 3) == Decimal("2.735")
